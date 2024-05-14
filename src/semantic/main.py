0890f0c54ce56bf6f6b13b998ec7ff2e6839ddc1
Alex Monras <amb.physis@gmail.com>
2024-05-14 22:44:34 +0200

    add cli entrypoint

---
+++ src/semantic/main.py
@@ -0,0 +1,37 @@
+from argparse import ArgumentParser
+from pathlib import Path
+
+from langchain_text_splitters import CharacterTextSplitter
+
+from semantic.consultant import Consultant
+from semantic.splitters.article_splitter import ArticleSplitter
+
+
+def init_parser():
+    parser = ArgumentParser(description="Semantic Query CLI")
+    parser.add_argument(
+        "--file",
+        type=str,
+        help="Path to the file to be uploaded",
+        required=True
+    )
+    parser.add_argument(
+        '--query',
+        type=str,
+        help="Query to be executed",
+        required=True
+    )
+    return parser
+
+
+def main():
+    parser = init_parser()
+    args = parser.parse_args()
+
+    splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    consultant = Consultant(file=Path(args.file), splitter=splitter)
+
+    docs = consultant.query(args.query)
+    for doc in docs:
+        print("=================")
+        print(doc.page_content)
