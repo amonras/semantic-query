47723bce654ed58c4ef8b33d0a00ca778b31d0c5
Alex Monras <amb.physis@gmail.com>
2024-11-27 17:48:51 +0100

    tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -10,17 +10,17 @@ class TestParser:
         parsed = parse(h4s)
 
         assert len(parsed) == 5
-        assert parsed[-1]['content'] == 'CAPÍTULO V: Ámbito de aplicación de los regímenes jurídicos civiles coexistentes en  el territorio nacional'
+        assert parsed[-1].content == 'CAPÍTULO V: Ámbito de aplicación de los regímenes jurídicos civiles coexistentes en  el territorio nacional'
 
     def test_parser_advances_tag_head_for_siblings(self, html_text):
         soup = BeautifulSoup(html_text, "html.parser")
-        h4s = soup.findAll('h4')[3:18]
-        parsed = parse(h4s)
+        tags = soup.findAll('h4')[3:18]
+        parsed = parse(tags)
 
         assert len(parsed) == 5
         # Check we have consumed all tags and the next one is
-        assert len(h4s) == 5
-        assert h4s[0].get('class')[0] == 'libro_num'
+        assert len(tags) == 5
+        assert tags[0].get('class')[0] == 'libro_num'
 
     def test_parser_returns_when_parent_found(self, html_text):
         soup = BeautifulSoup(html_text, "html.parser")
@@ -28,9 +28,7 @@ class TestParser:
         parsed = parse(tags, level='capitulo_num')[:100]
 
         assert len(parsed) == 5
-        assert len(parsed[0]['children']) == 2
-
-
+        assert len(parsed[0].children) == 2
 
     def test_parse_children(self, html_text):
         soup = BeautifulSoup(html_text, "html.parser")
@@ -38,22 +36,31 @@ class TestParser:
         parsed = parse(h4s)
 
         assert len(parsed) == 1
-        assert parsed[0]['level'] == 'titulo'
-        assert len(parsed[0]['children']) == 5
+        assert parsed[0].level == 'titulo'
+        assert len(parsed[0].children) == 5
 
     def test_parse_siblings_with_children(self, html_text):
         soup = BeautifulSoup(html_text, "html.parser")
         h4s = soup.findAll(['h4', 'h5', 'p'])
         parsed = parse(h4s, level='titulo_num')[0]
 
-        assert parsed['level'] == 'titulo'
-        assert parsed['children'][0]['level'] == 'capitulo'
-        assert parsed['children'][0]['children'][1]['level'] == 'articulo'
+        assert parsed.level == 'titulo'
+        assert parsed.children[0].level == 'capitulo'
+        assert parsed.children[0].children[1].level == 'articulo'
 
     def test_parse_paragraphs(self, html_text):
         soup = BeautifulSoup(html_text, "html.parser")
         h4s = soup.findAll(['h4', 'h5', 'p'])
         parsed = parse(h4s, level='anexo')
 
-        assert parsed[0]['level'] == 'document'
+        assert parsed[0].level == 'document'
+
+    def test_parse_entire_doc(self, html_text):
+        soup = BeautifulSoup(html_text, "html.parser")
+        tags = soup.findAll(['h4', 'h5', 'p'])
+        parsed = parse(tags, level='anexo')[0]
+
+        assert 'TÍTULO PRELIMINAR' in parsed.children[0].content
+        assert 'LIBRO PRIMERO' in parsed.children[1].content
+        assert 'LIBRO SEGUNDO' in parsed.children[2].content
 
