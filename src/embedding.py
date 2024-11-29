0898c363217fa42d64da6b20277aa0b4b4a67dc8
Alex Monras <amb.physis@gmail.com>
2024-11-29 17:34:09 +0100

    easily switch embedding models

--- src/embedding.py
+++ src/embedding.py
@@ -1,18 +1,22 @@
-from typing import List, Union, Tuple
+import argparse
+import configparser
+from typing import List, Union, Tuple, Optional
 
 from numpy import ndarray
 from sentence_transformers import SentenceTransformer
 from torch import Tensor
 
+from config import root_path
 from models.node import Node
 
 
 class Embedding:
-    def __init__(self):
+    def __init__(self, conf: Optional[configparser.ConfigParser] = None):
+        self.conf = conf or configparser.ConfigParser()
         # Load a pre-trained model
         self.model = SentenceTransformer(  # Lightweight, fast model
-            'all-MiniLM-L6-v2',
-            cache_folder='../cache/',
+            conf.get('embedding', 'model_name_or_path'),
+            cache_folder=root_path() / conf.get('embedding', 'cache_folder')
         )
 
     def embed_nodes(self, nodes: List[Node]) -> tuple[
